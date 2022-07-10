e09509919988818c9b62a228259887ea0286523f
Nuwan128 <nuwanpkumara128@gmail.com>
2022-07-10 20:18:52 +0530

    add another images

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+var i = 0;
+var time = 5000;
+
+var imges = [];
+
+imges[0] = '1.png';
+imges[1] = '2.png';
+imges[2] = '3.png';
+imges[3] = '4.png';
+
+
+// function changeImg(){
+//     document.slide.src = imges[i];
+
+//     if(i < imges.length -1){
+//         i++;
+//     }
+//     else{
+//         i = 0;
+//     }
+
+//     setTimeout("changeImg()",time)
+
+//     window.onload = changeImg;
+// }
+
+console.log(imges);
