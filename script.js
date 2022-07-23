f52a23017ead5d5decebf600c6c8ee4e17c048c5
Nuwan128 <nuwanpkumara128@gmail.com>
2022-07-23 19:37:09 +0530

    changes

--- script.js
+++ script.js
@@ -1,29 +1,101 @@
 
+//variable
 
-var i = 0;
-var time = 5000;
+const btnDiv = document.getElementById('navigation');
+const bgImage = document.getElementById('home');
+const btnList = document.getElementsByTagName('input');
+const btnLable = document.getElementsByTagName('label');
+const arrowDiv = document.getElementById('arrowDiv');
 
-var imges = [];
+let count = 0;
+let time = 5000;
+let imges = [];
 
-imges[0] = '1.png';
-imges[1] = '2.png';
-imges[2] = '3.png';
-imges[3] = '4.png';
+imges[0] = "url('/img/1.png') rgba(0, 0, 0, 0.19)";
+imges[1] = "url('/img/2.png') rgba(0, 0, 0, 0.19)";
+imges[2] = "url('/img/3.png') rgba(0, 0, 0, 0.19)";
+imges[3] = "url('/img/4.png') rgba(0, 0, 0, 0.19)";
+
+//events
+btnDiv.addEventListener('click',changeRadio);
+arrowDiv.addEventListener('click',arrowKeys)
+
+window.onload = changeImg;
 
 
-// function changeImg(){
-//     document.slide.src = imges[i];
+//--Funtions--
+//--change image--
 
-//     if(i < imges.length -1){
-//         i++;
-//     }
-//     else{
-//         i = 0;
-//     }
+function changeImg(){
 
-//     setTimeout("changeImg()",time)
+    btnList[count].checked = true;
+    bgImage.style.background = imges[count];
+    bgImage.style.transition = '1s';
+    
+    for (let i = 0; i < btnList.length; i++) {
+        
+        if(btnList[i].checked == true){
+            count++;
+            btnLable[i].style.background = 'rgb(235, 59, 112)';
+            btnLable[i].style.transform = 'scale(1.25)';
+            
+        }
+        else{
+            btnLable[i].style.background = '';
+            btnLable[i].style.transform = 'scale(1)';
+            
+        }
+        
+    } 
+    if(count > imges.length-1){
+        count = 0;
+    }
 
-//     window.onload = changeImg;
-// }
+    setTimeout("changeImg()",time);
+}
+   
 
-console.log(imges);
+
+
+ //manual btn
+
+ function changeRadio(e){
+    
+
+    if(e.target.className == 'radio btn-1'){
+        
+        bgImage.style.background = imges[0];
+        count = 0;
+    }
+    else if(e.target.className == 'radio btn-2'){
+        bgImage.style.background = imges[1];
+        count = 1;
+    }
+    else if(e.target.className == 'radio btn-3'){
+        bgImage.style.background = imges[2];
+        count = 2;
+    }
+    else if(e.target.className == 'radio btn-4'){
+        bgImage.style.background = imges[3];
+        count = 3;
+        
+    }
+ }
+
+//arrow keys
+function arrowKeys(e){
+    let img = 0;
+    if(e.target.className == 'arrow leftBtn'){
+        img = count--;
+        console.log(count);
+        bgImage.style.background = imges[img];
+        
+
+    }
+    else if(e.target.className == 'arrow rightBtn'){
+        img = count++;
+        console.log(count);
+        bgImage.style.background = imges[img];
+       
+    }
+}
